842d8a3f51b1a557e9fd40670cd90c47238e7f19
julian-perez <julian.perez@canary.is>
2018-03-12 17:23:45 -0400

    intentional break

--- app/src/main/java/com/example/julian/androidtestapp/MainActivity.java
+++ app/src/main/java/com/example/julian/androidtestapp/MainActivity.java
@@ -18,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        hello there, this will break
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
