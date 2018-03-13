e254f509f0d14d8ec0e2ea049b82899ac6d83035
julian-perez <julian.perez@canary.is>
2018-03-13 10:30:50 -0400

    new fix

--- app/src/main/java/com/example/julian/androidtestapp/MainActivity.java
+++ app/src/main/java/com/example/julian/androidtestapp/MainActivity.java
@@ -18,8 +18,6 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        fail test
-
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
